16b3c2c313a1c2be5ba7e0062b9419aecb6a5233
declankirk <declan.kirk@gmail.com>
2019-09-15 13:58:38 +1000

    image gen, api posting

--- bot.js
+++ bot.js
@@ -1,32 +1,106 @@
-var fs = require("fs");
-var text = fs.readFileSync("./templates.txt");
-var templates = text.toString().split("\r\n");
-text = fs.readFileSync("./nouns.txt");
-var nouns = text.toString().split("\r\n");
-text = fs.readFileSync("./adjectives.txt");
-var adjectives = text.toString().split("\r\n");
+const PAGE_ID = "111183643597806"
+const TOKEN = "EAACVS6jUj0QBAJxOM5uaZB6ydz8LhvuzUcZC1Io6BeqcaCXEXCWO4MxD7uBYsyU2GKFo3G3zMxmwISzUYTrv7xhQP6cl7gCpZCvc1pWnj1bZCnqd2jocCTENrJARiTvCDDDG2bdT779deCIesqq6BR25od3zRo52ZCpJjx2cKwZAyux0I9UAvb";
 
-var template = templates[Math.floor(Math.random()*templates.length)];
-var out = "";
+const FB = require('fb');
+FB.setAccessToken(TOKEN);
+const CRON = require("node-cron");
 
-for (var i = 0; i < template.length; i++) {
-    if (template.charAt(i) == '#') {
-        var noun = nouns[Math.floor(Math.random()*nouns.length)];
-        out += noun;
+
+FB.api(`${PAGE_ID}/photos`, "post", {
+    source: genImgURL()
+}, res => {
+    if (!res || res.error) {
+        console.log(!res ? "Error" : res.error);
+        return;
     }
-    else if (template.charAt(i) == '%') {
-        var adj = adjectives[Math.floor(Math.random()*adjectives.length)];
-        out += adj;
-    } else {
-        out += template.charAt(i);
+    var postID = res.post_id;
+    console.log("Posted: " + postID);
+});
+
+/**
+ * Generates title, length and views as string
+ */
+function genTitle() {
+    var fs = require("fs");
+    var text = fs.readFileSync("./templates.txt");
+    var templates = text.toString().split("\r\n");
+    text = fs.readFileSync("./nouns.txt");
+    var nouns = text.toString().split("\r\n");
+    text = fs.readFileSync("./adjectives.txt");
+    var adjectives = text.toString().split("\r\n");
+
+    var template = templates[Math.floor(Math.random()*templates.length)];
+    var out = "";
+
+    for (var i = 0; i < template.length; i++) {
+        if (template.charAt(i) == '#') {
+            var noun = nouns[Math.floor(Math.random()*nouns.length)];
+            out += noun;
+        }
+        else if (template.charAt(i) == '%') {
+            var adj = adjectives[Math.floor(Math.random()*adjectives.length)];
+            out += adj;
+        } else {
+            out += template.charAt(i);
+        }
     }
+
+    var mins = Math.floor(Math.random()*59);
+    var secs = ("0" + Math.floor(Math.random()*59)).slice(-2);
+    out += " | (" + mins + ":" + secs + ")";
+    out = wordWrap(out, 25);
+    var views = Math.floor(Math.random()*1000000);
+    views = views.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    out += "\n\n" + views + " views";
+
+    return out;
 }
 
-var mins = Math.floor(Math.random()*59);
-var secs = ("0" + Math.floor(Math.random()*59)).slice(-2);
-var views = Math.floor(Math.random()*1000000);
-views = views.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-out += " | (" + mins + ":" + secs + ")\n" + views + " views";
+/**
+ * Generates image url of text
+ */
+function genImgURL() {
+    var text2png = require('text2png');
+    var fs = require('fs');
+    fs.writeFileSync('out.png', text2png(genTitle(), {
+        color: 'black',
+        backgroundColor: '#f1f1f1',
+        font: '80px Sans',
+        padding: 100,
+        lineSpacing: 20,
+        output: "dataURL"
+    }));
 
+    return "./out.png";
+}
 
-console.log(out);
+/**
+ * Wraps text string
+ * Stolen from https://stackoverflow.com/questions/14484787/wrap-text-in-javascript
+ */
+function wordWrap(str, maxWidth) {
+    var newLineStr = "\n"; done = false; res = '';
+    while (str.length > maxWidth) {                 
+        found = false;
+        // Inserts new line at first whitespace of the line
+        for (i = maxWidth - 1; i >= 0; i--) {
+            if (testWhite(str.charAt(i))) {
+                res = res + [str.slice(0, i), newLineStr].join('');
+                str = str.slice(i + 1);
+                found = true;
+                break;
+            }
+        }
+        // Inserts new line at maxWidth position, the word is too long to wrap
+        if (!found) {
+            res += [str.slice(0, maxWidth), newLineStr].join('');
+            str = str.slice(maxWidth);
+        }
+
+    }
+    return res + str;
+}
+function testWhite(x) {
+    var white = new RegExp(/^\s$/);
+    return white.test(x.charAt(0));
+};
